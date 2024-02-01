2c6c6530364361cb7f5853de6fe2f1cb63102eef
MatteoSanson <matteo.sanson@gmail.com>
2024-02-01 18:47:58 +0100

    feat: route controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ComicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ComicController::class, 'index'])->name('comics');
