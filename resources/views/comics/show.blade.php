d62e4230261b56a0db1af8c7255449315919ca72
MatteoSanson <matteo.sanson@gmail.com>
2024-02-01 22:22:34 +0100

    feat: show detail card

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $comic->title }}</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <header>
+        <div class="container text-center py-5">
+            <h1>{{ $comic->title }}</h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+
+            <div class="card mb-3 container-lg">
+                <div class="row g-0">
+                    <div class="col-md-4 py-3">
+                        <img src="{{ $comic->thumb_img }}" class="img-fluid rounded-start" alt="{{ $comic->title }}">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <p class="card-text"><strong>Series:</strong> {{ $comic->series }}</p>
+                            <p class="card-text"><strong>Type:</strong> {{ $comic->type }}</p>
+                            <p class="card-text"><strong>Price:</strong> {{ $comic->price }}$</p>
+                            <p class="card-text"><strong>Sale Date:</strong> {{ $comic->sale_date }}</p>
+                            <p class="card-text"><strong>Description:</strong> {{ $comic->description }}</p>
+                            <p class="card-text"><strong>Artists:</strong> {{ $comic->artists }}</p>
+                            <p class="card-text"><strong>Writers:</strong> {{ $comic->writers }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <a href="{{ route('comics.index') }}" class="py-2">Back to comics list</a>
+        </div>
+    </main>
+</body>
+
+</html>
