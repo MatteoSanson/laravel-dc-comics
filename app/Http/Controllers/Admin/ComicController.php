5b61637c59f0676db86cf565dc67d16defbed2d3
MatteoSanson <matteo.sanson@gmail.com>
2024-02-02 15:58:17 +0100

    feat: function update comiccontroller

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -73,9 +73,13 @@ class ComicController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Comic $comic)
     {
-        //
+        $data = $request->all();
+        
+        $comic->update($data);
+
+        return redirect()->route('comics.show', $comic->id);
     }
 
     /**
