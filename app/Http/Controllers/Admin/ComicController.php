2599c5818bcbbf6b461694771f8f620b2d71da6d
MatteoSanson <matteo.sanson@gmail.com>
2024-02-05 21:55:15 +0100

    feat: bonus 1 validation store

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Comic;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Validator;
 
 class ComicController extends Controller
 {
@@ -32,21 +34,23 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
-            'title' => 'required|max:60',
-            'series' => 'required|max:100',
-            'type' => 'required|max:40',
-            'price' => 'required|numeric|min:0|regex:/^\d{1,8}(\.\d{2})?$/',
-            // 'sale_date' => 'nullable|date_format:Y-m-d',
-            'sale_date' => 'date_format:Y-m-d',
-            // 'description' => 'nullable',
-            'artists' => 'required|max:300',
-            'writers' => 'required|max:300',
-            // 'thumb_img' => 'nullable|url|ends_with:.jpeg,.png,.svg,.webp,.bmp|max:400',
-            'thumb_img' => 'url|ends_with:.jpeg,.png,.svg,.webp,.bmp|max:400',
-        ]);
+        // $request->validate([
+        //     'title' => 'required|max:60',
+        //     'series' => 'required|max:100',
+        //     'type' => 'required|max:40',
+        //     'price' => 'required|numeric|min:0|regex:/^\d{1,8}(\.\d{2})?$/',
+        //     // 'sale_date' => 'nullable|date_format:Y-m-d',
+        //     'sale_date' => 'date_format:Y-m-d',
+        //     // 'description' => 'nullable',
+        //     'artists' => 'required|max:300',
+        //     'writers' => 'required|max:300',
+        //     // 'thumb_img' => 'nullable|url|ends_with:.jpeg,.png,.svg,.webp,.bmp|max:400',
+        //     'thumb_img' => 'url|ends_with:.jpeg,.png,.svg,.webp,.bmp|max:400',
+        // ]);
 
-        $data = $request->all();
+        // $data = $request->all();
+
+        $data = $this->validation($request->all());
         
         $comic = new Comic();
         // $comic->title = $data['title'];
@@ -91,6 +95,7 @@ class ComicController extends Controller
     public function update(Request $request, Comic $comic)
     {
         $data = $request->all();
+        // $data = $this->validation($request->all());
         
         $comic->update($data);
 
@@ -104,5 +109,43 @@ class ComicController extends Controller
     {
         $comic->delete();
         return redirect()->route('comics.index');
+    }
+
+
+     /**
+     * funzione validation.
+     */
+    public function validation($data)
+    {
+        $validator = Validator::make($data, [
+            
+            'title' => 'required|min:2|max:60',
+            'series' => 'required|min:5|max:100',
+            'type' => 'required|min:5|max:40',
+            'price' => 'required|numeric|min:0|regex:/^\d{1,8}(\.\d{2})?$/',
+            'sale_date' => 'nullable|date_format:Y-m-d',
+            'description' => 'nullable',
+            'artists' => 'required|min:5|max:300',
+            'writers' => 'required|min:5|max:300',
+            'thumb_img' => 'nullable|url|ends_with:.jpeg,.jpg,.png,.svg,.webp,.bmp|max:400',
+        ],[
+            'title.min' => 'Il campo "title" deve essere lungo almeno :min caratteri.',
+            'title.max' => 'Il campo "title" non può superare i :max caratteri.',
+            'type.min' => 'Il campo "type" deve essere lungo almeno :min caratteri.',
+            'type.max' => 'Il campo "type" non può superare i :max caratteri.',
+            'series.min' => 'Il campo "series" deve essere lungo almeno :min caratteri.',
+            'series.max' => 'Il campo "series" non può superare i :max caratteri.',
+            'sale_date.date_format' => 'Il campo "date" deve essere in formato YYYY-MM--DD.',
+            'artists.min' => 'Il campo "artists" deve essere lungo almeno :min caratteri.',
+            'artists.max' => 'Il campo "artists" non può superare i :max caratteri.',
+            'writers.min' => 'Il campo "writers" deve essere lungo almeno :min caratteri.',
+            'writers.max' => 'Il campo "writers" non può superare i :max caratteri.',
+            'thumb_img.url' => 'Il campo "image" deve contenere una url.',
+            'thumb_img.ends_with' => 'Il campo "image" deve terminare con .jpeg,.jpg,.png,.svg,.webp,.bmp.',
+            'price.numeric' => 'Il campo "price" deve essere un numero',
+            'price.regex' => 'Il campo "price" deve essere in un formato valido, i numeri interi separati da "." e 2 cifre come decimali ',
+        ])->validate();
+
+        return $validator;
     }
 }
--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -24,51 +24,82 @@
 
             <div class="mb-3">
                 <label class="form-label">Title</label>
-                <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" required>
+                <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" required
+                    value="{{ old('title') }}">
                 @error('title')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-3">
                 <label class="form-label">Series</label>
-                <input type="text" class="form-control" name="series" required>
+                <input type="text" class="form-control @error('series') is-invalid @enderror" name="series" required
+                    value="{{ old('series') }}">
+                @error('series')
+                    <div class="alert
+                    alert-danger">{{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label class="form-label">Type</label>
-                <input type="text" class="form-control" name="type" required>
+                <input type="text" class="form-control @error('type') is-invalid @enderror" name="type" required
+                    value="{{ old('type') }}">
+                @error('type')
+                    <div class="alert alert-danger">{{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label class="form-label">Price</label>
-                <input type="text" class="form-control @error('price') is-invalid @enderror" name="price" required>
+                <input type="text" class="form-control @error('price') is-invalid @enderror" name="price" required
+                    value="{{ old('price') }}">
                 @error('price')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                    <div class="invalid-feedback">{{ $message }}
+                    </div>
                 @enderror
             </div>
             <div class="mb-3">
                 <div class="mb-3">
                     <label for="customInput" class="form-label">Date</label>
                     <input type="text" class="form-control @error('sale_date') is-invalid @enderror" id="customInput"
-                        name="sale_date" pattern="[A-Za-z0-9]{4}-[A-Za-z0-9]{2}-[A-Za-z0-9]{2}" placeholder="XXXX-XX-XX">
+                        name="sale_date" pattern="[A-Za-z0-9]{4}-[A-Za-z0-9]{2}-[A-Za-z0-9]{2}" placeholder="XXXX-XX-XX"
+                        value="{{ old('sale_date') }}">
                     @error('sale_date')
-                        <div class="invalid-feedback">{{ $message }}</div>
+                        <div class="invalid-feedback">{{ $message }}
+                        </div>
                     @enderror
                 </div>
             </div>
             <div class="mb-3">
                 <label class="form-label">Artists</label>
-                <input type="text" class="form-control" name="artists" required>
+                <input type="text" class="form-control @error('artists') is-invalid @enderror" name="artists" required
+                    value="{{ old('artists') }}">
+                @error('artists')
+                    <div class="invalid-feedback">{{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label class="form-label">Writers</label>
-                <input type="text" class="form-control" name="writers" required>
+                <input type="text" class="form-control @error('writers') is-invalid @enderror" name="writers" required
+                    value="{{ old('writers') }}">
+                @error('writers')
+                    <div class="invalid-feedback">{{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="exampleFormControlTextarea1" class="form-label">Description</label>
-                <textarea class="form-control" name="description" rows="6"></textarea>
+                <textarea class="form-control" name="description" rows="6">{{ old('description') }}</textarea>
             </div>
             <div class="mb-3">
                 <label class="form-label">Image</label>
-                <input type="text" class="form-control" name="thumb_img">
+                <input type="text" class="form-control @error('thumb_img') is-invalid @enderror" name="thumb_img"
+                    value="{{ old('thumb_img') }}">
+                @error('thumb_img')
+                    <div class="invalid-feedback">{{ $message }}
+                    </div>
+                @enderror
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
