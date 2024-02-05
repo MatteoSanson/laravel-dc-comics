681ab4fd9eda393df682790b029e7b186c8453c7
MatteoSanson <matteo.sanson@gmail.com>
2024-02-05 23:06:54 +0100

    feat: bonus2 validazioni con request

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Comic;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\StoreComicRequest;
+use App\Http\Requests\UpdateComicRequest;
 
 class ComicController extends Controller
 {
@@ -32,7 +34,7 @@ class ComicController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreComicRequest $request)
     {
         // $request->validate([
         //     'title' => 'required|max:60',
@@ -50,8 +52,9 @@ class ComicController extends Controller
 
         // $data = $request->all();
 
-        $data = $this->validation($request->all());
+        // $data = $this->validation($request->all());
         
+        $data = $request->validated();
         $comic = new Comic();
         // $comic->title = $data['title'];
         // $comic->series = $data['series'];
@@ -92,11 +95,12 @@ class ComicController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Comic $comic)
+    public function update(UpdateComicRequest $request, Comic $comic)
     {
         // $data = $request->all();
-        $data = $this->validation($request->all());
+        // $data = $this->validation($request->all());
         //
+        $data = $request->validated();
         $comic->update($data);
 
         return redirect()->route('comics.show', $comic->id);
@@ -112,40 +116,40 @@ class ComicController extends Controller
     }
 
 
-     /**
-     * funzione validation.
-     */
-    public function validation($data)
-    {
-        $validator = Validator::make($data, [
+    //  /**
+    //  * funzione validation.
+    //  */
+    // public function validation($data)
+    // {
+    //     $validator = Validator::make($data, [
             
-            'title' => 'required|min:2|max:60',
-            'series' => 'required|min:5|max:100',
-            'type' => 'required|min:5|max:40',
-            'price' => 'required|numeric|min:0|regex:/^\d{1,8}(\.\d{2})?$/',
-            'sale_date' => 'nullable|date_format:Y-m-d',
-            'description' => 'nullable',
-            'artists' => 'required|min:5|max:300',
-            'writers' => 'required|min:5|max:300',
-            'thumb_img' => 'nullable|url|ends_with:.jpeg,.jpg,.png,.svg,.webp,.bmp|max:400',
-        ],[
-            'title.min' => 'Il campo "title" deve essere lungo almeno :min caratteri.',
-            'title.max' => 'Il campo "title" non può superare i :max caratteri.',
-            'type.min' => 'Il campo "type" deve essere lungo almeno :min caratteri.',
-            'type.max' => 'Il campo "type" non può superare i :max caratteri.',
-            'series.min' => 'Il campo "series" deve essere lungo almeno :min caratteri.',
-            'series.max' => 'Il campo "series" non può superare i :max caratteri.',
-            'sale_date.date_format' => 'Il campo "date" deve essere in formato YYYY-MM--DD.',
-            'artists.min' => 'Il campo "artists" deve essere lungo almeno :min caratteri.',
-            'artists.max' => 'Il campo "artists" non può superare i :max caratteri.',
-            'writers.min' => 'Il campo "writers" deve essere lungo almeno :min caratteri.',
-            'writers.max' => 'Il campo "writers" non può superare i :max caratteri.',
-            'thumb_img.url' => 'Il campo "image" deve contenere una url.',
-            'thumb_img.ends_with' => 'Il campo "image" deve terminare con .jpeg,.jpg,.png,.svg,.webp,.bmp.',
-            'price.numeric' => 'Il campo "price" deve essere un numero',
-            'price.regex' => 'Il campo "price" deve essere in un formato valido, i numeri interi separati da "." e 2 cifre come decimali ',
-        ])->validate();
+    //         'title' => 'required|min:2|max:60',
+    //         'series' => 'required|min:5|max:100',
+    //         'type' => 'required|min:5|max:40',
+    //         'price' => 'required|numeric|min:0|regex:/^\d{1,8}(\.\d{2})?$/',
+    //         'sale_date' => 'nullable|date_format:Y-m-d',
+    //         'description' => 'nullable',
+    //         'artists' => 'required|min:5|max:300',
+    //         'writers' => 'required|min:5|max:300',
+    //         'thumb_img' => 'nullable|url|ends_with:.jpeg,.jpg,.png,.svg,.webp,.bmp|max:400',
+    //     ],[
+    //         'title.min' => 'Il campo "title" deve essere lungo almeno :min caratteri.',
+    //         'title.max' => 'Il campo "title" non può superare i :max caratteri.',
+    //         'type.min' => 'Il campo "type" deve essere lungo almeno :min caratteri.',
+    //         'type.max' => 'Il campo "type" non può superare i :max caratteri.',
+    //         'series.min' => 'Il campo "series" deve essere lungo almeno :min caratteri.',
+    //         'series.max' => 'Il campo "series" non può superare i :max caratteri.',
+    //         'sale_date.date_format' => 'Il campo "date" deve essere in formato YYYY-MM--DD.',
+    //         'artists.min' => 'Il campo "artists" deve essere lungo almeno :min caratteri.',
+    //         'artists.max' => 'Il campo "artists" non può superare i :max caratteri.',
+    //         'writers.min' => 'Il campo "writers" deve essere lungo almeno :min caratteri.',
+    //         'writers.max' => 'Il campo "writers" non può superare i :max caratteri.',
+    //         'thumb_img.url' => 'Il campo "image" deve contenere una url.',
+    //         'thumb_img.ends_with' => 'Il campo "image" deve terminare con .jpeg,.jpg,.png,.svg,.webp,.bmp.',
+    //         'price.numeric' => 'Il campo "price" deve essere un numero',
+    //         'price.regex' => 'Il campo "price" deve essere in un formato valido, i numeri interi separati da "." e 2 cifre come decimali ',
+    //     ])->validate();
 
-        return $validator;
-    }
+    //     return $validator;
+    // }
 }
---
+++ app/Http/Requests/UpdateComicRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateComicRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|min:2|max:60',
+            'series' => 'required|min:5|max:100',
+            'type' => 'required|min:5|max:40',
+            'price' => 'required|numeric|min:0|regex:/^\d{1,8}(\.\d{2})?$/',
+            'sale_date' => 'nullable|date_format:Y-m-d',
+            'description' => 'nullable',
+            'artists' => 'required|min:5|max:300',
+            'writers' => 'required|min:5|max:300',
+            'thumb_img' => 'nullable|url|ends_with:.jpeg,.jpg,.png,.svg,.webp,.bmp|max:400',
+        ];
+    }
+    
+    /**
+     * messaggio per gli errori.
+     *
+     * @return array<string, 
+     */
+    public function messages()
+    {
+        return [
+            'title.min' => 'Il campo "title" deve essere lungo almeno :min caratteri.',
+            'title.max' => 'Il campo "title" non può superare i :max caratteri.',
+            'type.min' => 'Il campo "type" deve essere lungo almeno :min caratteri.',
+            'type.max' => 'Il campo "type" non può superare i :max caratteri.',
+            'series.min' => 'Il campo "series" deve essere lungo almeno :min caratteri.',
+            'series.max' => 'Il campo "series" non può superare i :max caratteri.',
+            'sale_date.date_format' => 'Il campo "date" deve essere in formato YYYY-MM-DD.',
+            'artists.min' => 'Il campo "artists" deve essere lungo almeno :min caratteri.',
+            'artists.max' => 'Il campo "artists" non può superare i :max caratteri.',
+            'writers.min' => 'Il campo "writers" deve essere lungo almeno :min caratteri.',
+            'writers.max' => 'Il campo "writers" non può superare i :max caratteri.',
+            'thumb_img.url' => 'Il campo "image" deve contenere una url.',
+            'thumb_img.ends_with' => 'Il campo "image" deve terminare con .jpeg,.jpg,.png,.svg,.webp,.bmp.',
+            'price.numeric' => 'Il campo "price" deve essere un numero',
+            'price.regex' => 'Il campo "price" deve essere in un formato valido, i numeri interi separati da "." e 2 cifre come decimali',
+        ];
+    }
+}
