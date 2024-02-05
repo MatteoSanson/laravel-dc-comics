31771201b3ceae492e0f1be54c8548e3769f802c
MatteoSanson <matteo.sanson@gmail.com>
2024-02-05 15:45:06 +0100

    fix: layouts

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,55 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel DC Comics</title>
-    @vite('resources/js/app.js')
-</head>
+@section('header')
+    <div class="container text-center">
+        <h1 class="py-3">Laravel DC Comics List</h1>
+    </div>
+@endsection
 
-<body>
-    <header>
-        <div class="container text-center">
-            <h1 class="py-3">Laravel DC Comics List</h1>
+@section('main')
+    <div class="container">
+        <div>
+            <a href="{{ route('comics.create') }}">
+                <p>Create a new comic</p>
+            </a>
         </div>
-    </header>
-
-    <main>
-        <div class="container">
-            <div>
-                <a href="{{ route('comics.create') }}">
-                    <p>Create a new comic</p>
-                </a>
-            </div>
-            <ul class="d-flex gap-3 flex-wrap">
-                @foreach ($comics as $comic)
-                    <div class="card" style="width: 18rem;">
-                        <div class="cnt-img pt-3">
-                            <img src={{ $comic->thumb_img }} class="card-img-top img-fluid" alt={{ $comic->title }}>
+        <ul class="d-flex gap-3 flex-wrap">
+            @foreach ($comics as $comic)
+                <div class="card" style="width: 18rem;">
+                    <div class="cnt-img pt-3">
+                        <img src={{ $comic->thumb_img }} class="card-img-top img-fluid" alt={{ $comic->title }}>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title text-center pb-2" style="height: 50px">{{ $comic->title }}</h5>
+                        <div class="text-center pt-2">
+                            <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary btn-sm">Details</a>
+                            <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-primary btn-sm">Update</a>
                         </div>
-                        <div class="card-body">
-                            <h5 class="card-title text-center pb-2" style="height: 50px">{{ $comic->title }}</h5>
-                            <div class="text-center pt-2">
-                                <a href="{{ route('comics.show', $comic->id) }}"
-                                    class="btn btn-primary btn-sm">Details</a>
-                                <a href="{{ route('comics.edit', $comic->id) }}"
-                                    class="btn btn-primary btn-sm">Update</a>
-                            </div>
-                            <div class="text-center pt-2">
-                                <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <input type="submit" value="Delete" class="btn btn-danger btn-sm">
-                                </form>
-                            </div>
+                        <div class="text-center pt-2">
+                            <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" value="Delete" class="btn btn-danger btn-sm">
+                            </form>
                         </div>
                     </div>
-                @endforeach
-            </ul>
-        </div>
-    </main>
-</body>
+                </div>
+            @endforeach
+        </ul>
+    </div>
+@endsection
 
 </html>
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,55 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $comic->title }}</title>
-    @vite('resources/js/app.js')
-</head>
+@section('header')
+    <div class="container text-center py-5">
+        <h1>{{ $comic->title }}</h1>
+    </div>
+@endsection
 
-<body>
-    <header>
-        <div class="container text-center py-5">
-            <h1>{{ $comic->title }}</h1>
-        </div>
-    </header>
+@section('main')
+    <div class="container">
 
-    <main>
-        <div class="container">
+        <div class="card mb-3 container-lg">
+            <div class="row g-0">
+                <div class="col-md-4 py-3">
+                    <img src="{{ $comic->thumb_img }}" class="img-fluid rounded-start" alt="{{ $comic->title }}">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <p class="card-text"><strong>Series:</strong> {{ $comic->series }}</p>
+                        <p class="card-text"><strong>Type:</strong> {{ $comic->type }}</p>
+                        <p class="card-text"><strong>Price:</strong> {{ $comic->price }}$</p>
+                        <p class="card-text"><strong>Sale Date:</strong> {{ $comic->sale_date }}</p>
+                        <p class="card-text"><strong>Description:</strong> {{ $comic->description }}</p>
+                        <p class="card-text"><strong>Artists:</strong> {{ $comic->artists }}</p>
+                        <p class="card-text"><strong>Writers:</strong> {{ $comic->writers }}</p>
 
-            <div class="card mb-3 container-lg">
-                <div class="row g-0">
-                    <div class="col-md-4 py-3">
-                        <img src="{{ $comic->thumb_img }}" class="img-fluid rounded-start" alt="{{ $comic->title }}">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <p class="card-text"><strong>Series:</strong> {{ $comic->series }}</p>
-                            <p class="card-text"><strong>Type:</strong> {{ $comic->type }}</p>
-                            <p class="card-text"><strong>Price:</strong> {{ $comic->price }}$</p>
-                            <p class="card-text"><strong>Sale Date:</strong> {{ $comic->sale_date }}</p>
-                            <p class="card-text"><strong>Description:</strong> {{ $comic->description }}</p>
-                            <p class="card-text"><strong>Artists:</strong> {{ $comic->artists }}</p>
-                            <p class="card-text"><strong>Writers:</strong> {{ $comic->writers }}</p>
-
-                            <div class="pt-2 d-flex gap-3">
-                                <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-primary">Update</a>
-                                <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <input type="submit" value="Delete" class="btn btn-danger">
-                                </form>
-                            </div>
+                        <div class="pt-2 d-flex gap-3">
+                            <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-primary">Update</a>
+                            <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" value="Delete" class="btn btn-danger">
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-
-            <a href="{{ route('comics.index') }}" class="py-2">Back to comics list</a>
         </div>
-    </main>
-</body>
 
-</html>
+        <a href="{{ route('comics.index') }}" class="py-2">Back to comics list</a>
+    </div>
+@endsection
